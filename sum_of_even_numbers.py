74679a9e486f25083ce9f4cca8331fcc647e28d5
Rubi Leandro <93342175+rubileandro@users.noreply.github.com>
2022-01-04 13:44:35 +0200

    Create sum_of_even_numbers.py

---
+++ sum_of_even_numbers.py
@@ -0,0 +1,13 @@
+#Write your code below this row 👇
+
+# Instructions: You are going to write a program that calculates the sum of all the even numbers from 1 to 100. Thus, the first even number would be 2 and the last one is 100: i.e. 2 + 4 + 6 + 8 +10 ... + 98 + 100
+
+# Important, there should only be 1 print statement in your console output. It should just print the final total and not every step of the calculation.
+
+# use range()
+
+total = 0
+
+for number in range(0, 101, 2):
+    total += number
+print(total)
