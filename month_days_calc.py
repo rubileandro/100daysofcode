d1569e3fb52f8bef83cd8b4e0b92ada9d50ec846
Rubi Leandro <93342175+rubileandro@users.noreply.github.com>
2022-02-12 11:25:41 +0200

    Create month_days_calc.py

--- month_days_calc.py
+++ month_days_calc.py
@@ -11,14 +11,13 @@ def is_leap(year):
     return False
 
 def days_in_month(in_year, in_month):
+  if month > 12 or month < 1:
+    return 'Invalid month'
   month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-  # If year is a leap year
   if is_leap(year) == True and month == 2:
-      month_days[1] = 29
-      return month_days[1]
+    return 29
   return month_days[month -1]
-
-#🚨 Do NOT change any of the code below 
+  
 year = int(input("Enter a year: "))
 month = int(input("Enter a month: "))
 days = days_in_month(year, month)
