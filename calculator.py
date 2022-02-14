bb83efb1c69e5e2a19a8e47ad626ab194d6e6745
Rubi Leandro <93342175+rubileandro@users.noreply.github.com>
2022-02-14 12:36:09 +0200

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,32 @@
+# Add
+def add(n1, n2):
+  return n1 + n2
+
+# Subtract
+def subtract(n1, n2):
+  return n1 - n2
+
+# Multiply
+def multiply(n1, n2):
+  return n1 * n2
+
+# Divide
+def divide(n1, n2):
+  return n1 / n2
+
+operations = {
+  "+": add,
+  "-": subtract,
+  "*": multiply,
+  "/": divide
+}
+
+num1 = int(input('What\'s the first number?: \n'))
+for operation in operations:
+  print(operation)
+operation_symbol = input('Pick an operation from the line above: ')
+num2 = int(input('What\'s the second number?: \n'))
+calculation_function = operations[operation_symbol]
+answer = calculation_function(num1, num2)
+
+print(f'{num1} {operation_symbol} {num2} = {answer}') 
