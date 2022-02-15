1c233663848f34ad615a9c6dac27b9ab28fba4e7
Rubi Leandro <93342175+rubileandro@users.noreply.github.com>
2022-02-15 18:48:56 +0200

    Update calculator.py
    Functionality added to continue working on the previous calculation.

--- calculator.py
+++ calculator.py
@@ -1,4 +1,3 @@
-# Add
 def add(n1, n2):
   return n1 + n2
 
@@ -21,12 +20,29 @@ operations = {
   "/": divide
 }
 
-num1 = int(input('What\'s the first number?: \n'))
-for operation in operations:
-  print(operation)
-operation_symbol = input('Pick an operation from the line above: ')
-num2 = int(input('What\'s the second number?: \n'))
-calculation_function = operations[operation_symbol]
-answer = calculation_function(num1, num2)
+def calculator():
+  num1 = int(input('What\'s the first number?: \n'))
+  for operation in operations:
+    print(operation)
+  should_continue = True
 
-print(f'{num1} {operation_symbol} {num2} = {answer}') 
+  while should_continue:
+    operation_symbol = input('Pick an operation: ')
+    num2 = int(input('What\'s the next number?: \n'))
+    calculation_function = operations[operation_symbol]
+    answer = calculation_function(num1, num2)
+
+  print(f'{num1} {operation_symbol} {num2} = {answer}')
+
+  operation_symbol = input('Pick another operation')
+  num3 = int(input('What\'s the next number?: '))
+  calculation_function = operations[operation_symbol]
+  second_answer = calculation_function(calculation_function(num1, num2), num3)
+
+  print(f'{first_answer} {operation_symbol} {num3} = {second_answer}')
+
+  if input(f'Type "y" to continue with {answer}, or type "n" to exit.: ') == "y":
+    num1 = answer 
+  else:
+    should_continue = False
+    calculator()
