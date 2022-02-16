bc967aca566ef732524004c18a8b3f0be993c704
Rubi Leandro <93342175+rubileandro@users.noreply.github.com>
2022-02-16 14:20:22 +0200

    Update calculator.py
    Fixed some bugs.

--- calculator.py
+++ calculator.py
@@ -1,4 +1,3 @@
-from art import logo
 def add(n1, n2):
   return n1 + n2
 
@@ -22,37 +21,22 @@ operations = {
 }
 
 def calculator():
-  print(logo)
-  num1 = float(input('What\'s the first number?: \n'))
+  num1 = int(input('What\'s the first number?: \n'))
   for operation in operations:
     print(operation)
   should_continue = True
 
   while should_continue:
     operation_symbol = input('Pick an operation: ')
-    num2 = float(input('What\'s the next number?: \n'))
+    num2 = int(input('What\'s the next number?: \n'))
     calculation_function = operations[operation_symbol]
     answer = calculation_function(num1, num2)
 
-    print(f'{num1} {operation_symbol} {num2} = {answer}')
-
-    if input(f'Type "y" to continue with {answer}, or type "n" to exit.: ') == "y":
-      num1 = answer 
-    else:
-      should_continue = False
-      calculator()
-
-calculator()
-
-  operation_symbol = input('Pick another operation')
-  num3 = int(input('What\'s the next number?: '))
-  calculation_function = operations[operation_symbol]
-  second_answer = calculation_function(calculation_function(num1, num2), num3)
-
-  print(f'{first_answer} {operation_symbol} {num3} = {second_answer}')
+  print(f'{num1} {operation_symbol} {num2} = {answer}')
 
   if input(f'Type "y" to continue with {answer}, or type "n" to exit.: ') == "y":
     num1 = answer 
   else:
     should_continue = False
-    calculator()
+
+calculator()
