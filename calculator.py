2d1476db91d65cb811b5af2ca44f4337f4ff54c3
Rubi Leandro <93342175+rubileandro@users.noreply.github.com>
2022-02-16 14:18:44 +0200

    Update calculator.py
    Fixed some bugs.

--- calculator.py
+++ calculator.py
@@ -1,3 +1,4 @@
+from art import logo
 def add(n1, n2):
   return n1 + n2
 
@@ -21,18 +22,27 @@ operations = {
 }
 
 def calculator():
-  num1 = int(input('What\'s the first number?: \n'))
+  print(logo)
+  num1 = float(input('What\'s the first number?: \n'))
   for operation in operations:
     print(operation)
   should_continue = True
 
   while should_continue:
     operation_symbol = input('Pick an operation: ')
-    num2 = int(input('What\'s the next number?: \n'))
+    num2 = float(input('What\'s the next number?: \n'))
     calculation_function = operations[operation_symbol]
     answer = calculation_function(num1, num2)
 
-  print(f'{num1} {operation_symbol} {num2} = {answer}')
+    print(f'{num1} {operation_symbol} {num2} = {answer}')
+
+    if input(f'Type "y" to continue with {answer}, or type "n" to exit.: ') == "y":
+      num1 = answer 
+    else:
+      should_continue = False
+      calculator()
+
+calculator()
 
   operation_symbol = input('Pick another operation')
   num3 = int(input('What\'s the next number?: '))
