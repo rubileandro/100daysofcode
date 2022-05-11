cf86fead4b1272e7d1ffac276fb8e7254e4264d8
Rubi Leandro <93342175+rubileandro@users.noreply.github.com>
2022-05-11 07:28:37 +0200

    Update roller_coaster.py

--- roller_coaster.py
+++ roller_coaster.py
@@ -11,6 +11,8 @@ if height >= 120:
   elif age <= 18:
     bill = 7
     print("Youth tickets are $7.")
+  elif age >= 45 and <= 55:
+    print("Everything is going to be ok. Have a free ride on us!") 
   else:
     bill = 12
     print("Adult tickets are $12.")
