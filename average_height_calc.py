79cfc7c48fb3cde282ae2ad343cae6dbd8e78e9a
Rubi Leandro <93342175+rubileandro@users.noreply.github.com>
2022-05-18 08:00:09 +0200

    Update average_height_calc.py
    Instructions were: 
    You are going to write a program that calculates the average student height from a List of heights.
    
    You should not use the sum() or len() functions in your answer. You should try to replicate their functionality using what you have learnt about for loops.

--- average_height_calc.py
+++ average_height_calc.py
@@ -1,24 +1,16 @@
-# 🚨 Don't change the code below 👇
 student_heights = input("Input a list of student heights ").split()
 for n in range(0, len(student_heights)):
   student_heights[n] = int(student_heights[n])
-# 🚨 Don't change the code above 👆
-#Write your code below this row 👇
 
-# How long is the list?
-length_of_list = 0
+total_height = 0
 for height in student_heights:
-    length_of_list += 1
-#print or return?
-print (length_of_list)
-        
-#Adding the numbers using a loop
-combined_heights = 0
-for height in student_heights:
-    combined_heights += height
+  total_height += height
+#print(total_height)
 
-# Calc average total / number of items
-average_height = combined_heights / length_of_list
+number_of_students = 0
+for student in student_heights:
+  number_of_students += 1
+#print(number_of_students)
 
-# Round result and give result
-print(round(average_height))
+average_height = round(total_height / number_of_students)
+print(average_height)
