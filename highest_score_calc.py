405dc958f973122b3c4fb799e2140a204e02fb0d
Rubi Leandro <93342175+rubileandro@users.noreply.github.com>
2022-05-19 15:04:44 +0200

    Update highest_score_calc.py
    Instructions were: 
    You are going to write a program that calculates the highest score from a List of scores.
    
    Important you are not allowed to use the max or min functions.

--- highest_score_calc.py
+++ highest_score_calc.py
@@ -1,27 +1,11 @@
-# 🚨 Don't change the code below 👇
 student_scores = input("Input a list of student scores ").split()
 for n in range(0, len(student_scores)):
   student_scores[n] = int(student_scores[n])
 print(student_scores)
-# 🚨 Don't change the code above 👆
 
-#Write your code below this row 👇
-
-# w/o max or min functions
-
-#loop
 highest_score = 0
 
 for score in student_scores:
-    if score > highest_score:
-        highest_score = score
-#print(highest_score)
-
-# case of two or more high scores?
-
-# print f string?
-print(f'The highest score in the class is: {highest_score}')
-
-#instructions were;
-# #You are going to write a program that calculates the highest score from a List of scores.
-# Important you are not allowed to use the max or min functions. The output words must match the example. i.e
+  if score > highest_score:
+    highest_score = score
+print(f"The highest_score in the class is:\n{highest_score}.")
