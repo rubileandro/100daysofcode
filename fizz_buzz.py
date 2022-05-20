01a51d465c427e1fd1bc084381a91a0b972d7909
Rubi Leandro <93342175+rubileandro@users.noreply.github.com>
2022-05-20 07:07:19 +0200

    Update fizz_buzz.py

--- fizz_buzz.py
+++ fizz_buzz.py
@@ -1,10 +1,21 @@
-num = 0
-for num in range(1, 101):
-    if num %3 == 0 and num %5 == 0:
-        print('FizzBuzz')
-    elif num % 3 == 0:
-        print('Fizz')
-    elif num % 5 == 0:
-        print('Buzz')
-    else:
-        print(num)
+# Instructions 
+# You are going to write a program that automatically prints the solution to the FizzBuzz game.
+
+# Your program should print each number from 1 to 100 in turn.
+
+# When the number is divisible by 3 then instead of printing the number it should print "Fizz".
+
+# When the number is divisible by 5, then instead of printing the number it should print "Buzz".`
+
+# And if the number is divisible by both 3 and 5 e.g. 15 then instead of the number it should print "FizzBuzz"
+
+n = 0
+for n in range(1, 101):
+	if n % 3 == 0 and n % 5 == 0:
+		print("FizzBuzz")
+	elif n % 3 == 0:
+		print("Fizz")
+	elif n % 5 == 0:
+		print("Buzz")
+	else:
+		print(n)
