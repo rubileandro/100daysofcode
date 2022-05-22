688651d8fcd0e2575e9a48327651eb962c9011ca
Rubi Leandro <93342175+rubileandro@users.noreply.github.com>
2022-05-22 11:58:53 +0200

    Create reborg.py

---
+++ reborg.py
@@ -0,0 +1,16 @@
+# Reborg's World
+# Lessons 1
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+turn_left()
+move()
+turn_right()
+move()
+turn_right()
+move()
+turn_right()
+move()
