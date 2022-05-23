8677518753c26f6794eb1bc3bd8f4c647741daae
Rubi Leandro <93342175+rubileandro@users.noreply.github.com>
2022-05-23 07:50:51 +0200

    Update reborg.py

--- reborg.py
+++ reborg.py
@@ -14,3 +14,50 @@ turn_right()
 move()
 turn_right()
 move()
+
+# Lesson 2 Hurle 1
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+def jump():
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+    
+move()    
+jump()
+move()
+jump()
+move()
+jump()
+move()
+jump()
+move()
+jump()
+move()
+jump()
+
+# alternative solution to hurdle 1
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+def jump():
+    move()
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()    
+
+for step in range(6):
+    jump()
