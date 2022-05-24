6f9addef399a34be93667917f86db4a504e0258e
Rubi Leandro <93342175+rubileandro@users.noreply.github.com>
2022-05-24 12:48:46 +0200

    Update reborg.py

--- reborg.py
+++ reborg.py
@@ -61,3 +61,22 @@ def jump():
 
 for step in range(6):
     jump()
+    
+# hurdle 2
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+def jump():
+    move()
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+    
+while not at_goal():
+    jump()
