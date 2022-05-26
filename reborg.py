365f1c504f79268ca4bae9bbb3b7a527147b149d
Rubi Leandro <93342175+rubileandro@users.noreply.github.com>
2022-05-26 07:31:25 +0200

    Update reborg.py

--- reborg.py
+++ reborg.py
@@ -103,3 +103,26 @@ while not at_goal():
         jump()
     else:
         move()
+
+# Hurdle 4 
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+def jump():
+    while wall_on_right():
+        move()
+    turn_right()
+    move()
+    turn_right()
+    while front_is_clear():
+        move()
+    turn_left()
+    
+while not at_goal():
+    if wall_in_front():
+        turn_left()
+        jump()
+    else:
+        move()
