8d83dbf00e408056828d8f4953ad6103f3376fc7
Rubi Leandro <93342175+rubileandro@users.noreply.github.com>
2022-05-29 08:49:03 +0200

    Create fucntions_with_inputs.py

---
+++ fucntions_with_inputs.py
@@ -0,0 +1,15 @@
+# Simple function
+# def greet():
+#   print("Hello.")
+#   print("How do you do?")
+#   print("Isn't the weather nice today?")
+
+# greet()
+
+# Function that allows for input
+
+def greet_with_name(name):
+  print(f"Hello {name}")
+  print(f"How do you do? {name}")
+
+greet_with_name("Rubi")
