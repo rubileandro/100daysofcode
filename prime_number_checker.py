d9840d2563a5b2e091306552da20757da300fc05
Rubi Leandro <93342175+rubileandro@users.noreply.github.com>
2022-05-31 07:33:41 +0200

    Update prime_number_checker.py
    Instructions were:
    You need to write a function that checks whether if the number passed into it is a prime number or not.

--- prime_number_checker.py
+++ prime_number_checker.py
@@ -1,20 +1,12 @@
-#Write your code below this line 👇
-
-def prime_checker(number):
-	is_prime = True
-	for i in range(2, number):
-		if number % i ==0:
-			is_prime = False
-	if is_prime:
-		print("It's a prime number.")
-	else:
-		print("It's not a prime number.")            
-
-
-#Write your code above this line 👆
-    
-#Do NOT change any of the code below👇
+ def prime_checker(num):
+    is_prime = True
+    for i in range(2, num):
+        if num % i == 0:
+            is_prime = False
+    if is_prime:
+        print("It's a prime number.")
+    else:
+    	print("It's not a prime number.")
+    	
 n = int(input("Check this number: "))
-prime_checker(number=n)
- 
-  
+prime_checker(num=n)
