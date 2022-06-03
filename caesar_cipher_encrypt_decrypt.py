6841d051dfb45e66cf664ccb6b30132440dd1cb6
Rubi Leandro <93342175+rubileandro@users.noreply.github.com>
2022-06-03 12:19:10 +0200

    Update caesar_cipher_encrypt_decrypt.py

--- caesar_cipher_encrypt_decrypt.py
+++ caesar_cipher_encrypt_decrypt.py
@@ -4,23 +4,14 @@ direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
-def encrypt(plain_text, shift_amount):
-  cipher_text = ""
-  for letter in plain_text:
+def caesar(start_text, shift_amount, cipher_direction):
+  end_text = ""
+  if cipher_direction == "decode":
+    shift_amount *= -1
+  for letter in start_text:
     position = alphabet.index(letter)
     new_position = position + shift_amount
-    cipher_text += alphabet[new_position]
-  print(f"The encoded text is {cipher_text}")
+    end_text += alphabet[new_position]
+  print(f"The {cipher_direction}d text is {end_text}.")
 
-def decrypt(cipher_text, shift_amount):
-  plain_text = ""
-  for letter in cipher_text:
-    position = alphabet.index(letter)
-    new_position = position - shift_amount
-    plain_text += alphabet[new_position]
-  print(f"The decoded text is {plain_text}")
-
-if direction == "encode":
-  encrypt(plain_text=text, shift_amount=shift)
-elif direction == "decode":
-  decrypt(cipher_text=text, shift_amount=shift)
+caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
