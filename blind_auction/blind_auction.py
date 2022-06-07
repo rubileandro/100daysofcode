71f31a9990ef6432ec5f665e125b2b66283f9b8a
Rubi Leandro <93342175+rubileandro@users.noreply.github.com>
2022-06-07 09:16:00 +0200

    Update blind_auction.py

--- blind_auction/blind_auction.py
+++ blind_auction/blind_auction.py
@@ -1,28 +1,27 @@
 from replit import clear
 from art import logo
-#HINT: You can call clear() to clear the output in the console.
 print(logo)
-# name = input('What is your name? Please enter your name. \n')
-# bid = input('What is your bid? Pleae enter your bid \n')
 
-#dict to keep track
-name_and_bids = {}
-# name_and_bids[name] = bid
-# print(name_and_bids)
-keepGoing = True
-while keepGoing:
-  #def bid_app():
-    #print(logo)
-    name = input('What is your name? Please enter your name. \n')
-    bid = input('What is your bid? Pleae enter your bid \n')
-    name_and_bids[name] = bid
-    #print(name_and_bids)
-    more_players = input('Are there any other bidders? Please types "Yes" or "No". \n').lower()
-    if more_players == 'yes':
-      clear()
-      keepGoing = True
-    else:
-      keepGoing = False
-      max_bid = max(name_and_bids, key=name_and_bids.get)
-    #print('You are the winner! Congratulations')
-      print(f'{max_bid} are the winner! Congratulations') 
+bids = {}
+bidding_finished = False
+
+def find_highest_bidder(bidding_record):
+  highest_bid = 0
+  winner = ""
+  for bidder in bidding_record:
+    bid_amount = bidding_record[bidder]
+    if bid_amount > highest_bid:
+      highest_bid = bid_amount
+      winner = bidder
+  print(f"The winner is {winner} with a bid of ${highest_bid}.")
+
+while not bidding_finished:
+  name = input("What is your name?\n")
+  price = int(input("What is your price?\n"))
+  bids[name] = price
+  should_continue = input("Are there any other bidders? Type 'yes' or 'no'.\n")
+  if should_continue == 'no':
+    bidding_finished == True
+    find_highest_bidder(bids)
+  elif should_continue == "yes":
+    clear()
