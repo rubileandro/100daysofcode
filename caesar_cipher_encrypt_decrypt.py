dfcb58c8e99ecbc94852b75fc6cccb353b3068bb
Rubi Leandro <93342175+rubileandro@users.noreply.github.com>
2022-06-10 09:58:27 +0200

    Update caesar_cipher_encrypt_decrypt.py

--- caesar_cipher_encrypt_decrypt.py
+++ caesar_cipher_encrypt_decrypt.py
@@ -5,13 +5,13 @@ text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
 def caesar(start_text, shift_amount, cipher_direction):
-  end_text = ""
-  if cipher_direction == "decode":
+  if direction == "decode":
     shift_amount *= -1
+  end_text = ""
   for letter in start_text:
     position = alphabet.index(letter)
     new_position = position + shift_amount
     end_text += alphabet[new_position]
-  print(f"The {cipher_direction}d text is {end_text}.")
+  print(f"The {cipher_direction}d text is {end_text}")
 
 caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
