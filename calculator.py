158de91bfa7e9e780ec49b7eb5cb6749239edb56
Rubi Leandro <93342175+rubileandro@users.noreply.github.com>
2022-06-10 11:27:09 +0200

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -40,3 +40,42 @@ def calculator():
     should_continue = False
 
 calculator()
+
+# Alternative 
+
+# Addition
+def add(n1, n2):
+  return n1 + n2
+  
+# Subtraction
+def sub(n1, n2):
+  return n1 - n2
+  
+# Multiplication 
+def multi(n1, n2):
+  return n1 * n2
+
+# Division
+def divide(n1, n2):
+  return n1 / n2
+
+# Exponent
+def exponent(n1, n2):
+  return n1 ** n2
+
+operations = {
+  "+": add,
+  "-": sub,
+  "*": multi,
+  "/": divide,
+}
+
+num1 = int(input("What's the first number?: "))
+for symbol in operations:
+  print(symbol)
+operation_symbol = input("choose an operation from the line above: ")
+num2 = int(input("What's the second number?: "))
+calculation_function = operations[operation_symbol]
+answer = calculation_function(num1, num2)
+
+print(f"{num1} {operation_symbol} {num2} = {answer}")
