7542f4b12251a63b7ae6d9c07ea455d784a799cb
Rubi Leandro <93342175+rubileandro@users.noreply.github.com>
2022-06-11 09:12:40 +0200

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,45 +1,45 @@
-def add(n1, n2):
-  return n1 + n2
+# def add(n1, n2):
+#   return n1 + n2
 
-# Subtract
-def subtract(n1, n2):
-  return n1 - n2
+# # Subtract
+# def subtract(n1, n2):
+#   return n1 - n2
 
-# Multiply
-def multiply(n1, n2):
-  return n1 * n2
+# # Multiply
+# def multiply(n1, n2):
+#   return n1 * n2
 
-# Divide
-def divide(n1, n2):
-  return n1 / n2
+# # Divide
+# def divide(n1, n2):
+#   return n1 / n2
 
-operations = {
-  "+": add,
-  "-": subtract,
-  "*": multiply,
-  "/": divide
-}
+# operations = {
+#   "+": add,
+#   "-": subtract,
+#   "*": multiply,
+#   "/": divide
+# }
 
-def calculator():
-  num1 = int(input('What\'s the first number?: \n'))
-  for operation in operations:
-    print(operation)
-  should_continue = True
+# def calculator():
+#   num1 = int(input('What\'s the first number?: \n'))
+#   for operation in operations:
+#     print(operation)
+#   should_continue = True
 
-  while should_continue:
-    operation_symbol = input('Pick an operation: ')
-    num2 = int(input('What\'s the next number?: \n'))
-    calculation_function = operations[operation_symbol]
-    answer = calculation_function(num1, num2)
+#   while should_continue:
+#     operation_symbol = input('Pick an operation: ')
+#     num2 = int(input('What\'s the next number?: \n'))
+#     calculation_function = operations[operation_symbol]
+#     answer = calculation_function(num1, num2)
 
-  print(f'{num1} {operation_symbol} {num2} = {answer}')
+#   print(f'{num1} {operation_symbol} {num2} = {answer}')
 
-  if input(f'Type "y" to continue with {answer}, or type "n" to exit.: ') == "y":
-    num1 = answer 
-  else:
-    should_continue = False
+#   if input(f'Type "y" to continue with {answer}, or type "n" to exit.: ') == "y":
+#     num1 = answer 
+#   else:
+#     should_continue = False
 
-calculator()
+# calculator()
 
 # Alternative 
 
@@ -76,6 +76,13 @@ for symbol in operations:
 operation_symbol = input("choose an operation from the line above: ")
 num2 = int(input("What's the second number?: "))
 calculation_function = operations[operation_symbol]
-answer = calculation_function(num1, num2)
+first_answer = calculation_function(num1, num2)
 
-print(f"{num1} {operation_symbol} {num2} = {answer}")
+print(f"{num1} {operation_symbol} {num2} = {first_answer}")
+
+operation_symbol = input("choose another operation: ")
+num3 = int(input("What's the next number?: "))
+calculation_function = operations[operation_symbol]
+second_answer = calculation_function(calculation_function(num1, num2), num3)
+
+print(f"{first_answer} {operation_symbol} {num3} = {second_answer}")
