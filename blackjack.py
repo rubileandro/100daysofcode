ea8167daf44c07c7fd878f45cb2e8dda532f5994
Rubi Leandro <93342175+rubileandro@users.noreply.github.com>
2022-06-14 07:20:00 +0200

    Update blackjack.py

--- blackjack.py
+++ blackjack.py
@@ -1,10 +1,16 @@
 import random
 
-cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
-
 def deal_card():
+  cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
   card_dealt = random.choice(cards)
   return card_dealt
 
-selected_card = deal_card()
-print(selected_card)
+user_cards = []
+computer_cards = []
+
+for entry in range(2):
+  user_cards.append(deal_card())
+  computer_cards.append(deal_card())
+
+print(user_cards)
+print(computer_cards)
