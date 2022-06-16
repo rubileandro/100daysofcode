a61d94b704e8102f7b7cd03d10066af4f10e9238
Rubi Leandro <93342175+rubileandro@users.noreply.github.com>
2022-06-16 07:30:36 +0200

    Update blackjack.py

--- blackjack.py
+++ blackjack.py
@@ -1,12 +1,11 @@
 import random
 
-# Function to deal cards
+# deal cards
 def deal_card():
   cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
   card_dealt = random.choice(cards)
   return card_dealt
 
-# Deal out cards
 user_cards = []
 computer_cards = []
 
@@ -17,8 +16,11 @@ for entry in range(2):
 print(user_cards)
 print(computer_cards)
 
+# calculate score
 def calculate_score(list_of_cards):
+  if (11 and 10 in list_of_cards):
+    return 0
   return sum(list_of_cards)
-
+  
 user_score = calculate_score(user_cards)
 print(user_score)
