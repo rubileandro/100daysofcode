90508e3ae422484439c8103d4b8f6c7c99aa508f
Rubi Leandro <93342175+rubileandro@users.noreply.github.com>
2022-06-19 08:50:39 +0200

    Update blackjack.py

--- blackjack.py
+++ blackjack.py
@@ -1,22 +1,10 @@
 import random
 
-# deal cards
 def deal_card():
   cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
   card_dealt = random.choice(cards)
   return card_dealt
 
-user_cards = []
-computer_cards = []
-
-for entry in range(2):
-  user_cards.append(deal_card())
-  computer_cards.append(deal_card())
-
-print(user_cards)
-print(computer_cards)
-
-# calculate score
 def calculate_score(list_of_cards):
   """Take a list of cards and return the score calculated from the cards"""
   #if 11 in list_of_cards and 10 in list_of_cards and len(list_of_cards) == 2:
@@ -35,10 +23,21 @@ for entry in range(2):
   user_cards.append(deal_card())
   computer_cards.append(deal_card())
 
-user_score = calculate_score(user_cards)
-computer_score = calculate_score(computer_cards)
-print(f" Your cards: {user_cards}, current score: {user_score}")
-print(f" Computer's first card: {computer_cards[0]}")
+while not end_of_game:
+  user_score = calculate_score(user_cards)
+  computer_score = calculate_score(computer_cards)
+  print(f" Your cards: {user_cards}, current score: {user_score}")
+  print(f" Computer's first card: {computer_cards[0]}")
+  
+  if user_score == 0 or computer_score == 0 or user_score > 21:
+    end_of_game = True
+  else:
+    deal_another_card = input("Type 'y' to get another card, type 'n' to pass.\n")
+    if deal_another_card == "y":
+      user_cards.append(deal_card())
+    else:
+      end_of_game = True
 
-if user_score == 0 or computer_score == 0 or user_score > 21:
-  end_of_game = True
+while computer_score != 0 and computer_score < 17:
+  computer_cards.append(deal_card())
+  computer_score = calculate_score(computer_cards)
