4b73720872e044abe7f113599ac162a3e4a517c5
Rubi Leandro <93342175+rubileandro@users.noreply.github.com>
2022-06-23 12:13:24 +0200

    Create guess_the_number.py

---
+++ guess_the_number.py
@@ -0,0 +1,20 @@
+#Number Guessing Game Objectives:
+
+# Include an ASCII art logo.
+# Allow the player to submit a guess for a number between 1 and 100.
+# Check user's guess against actual answer. Print "Too high." or "Too low." depending on the user's answer. 
+# If they got the answer correct, show the actual answer to the player.
+# Track the number of turns remaining.
+# If they run out of turns, provide feedback to the player. 
+# Include two different difficulty levels (e.g., 10 guesses in easy mode, only 5 guesses in hard mode).
+
+from random import randint
+from art import logo
+
+print(logo)
+print("Welcome to the number guessing game!")
+
+ANSWER = randint(1, 100)
+print(ANSWER)
+          
+guess = input("Guess a number between 1-100:")
