09e1345670ff1e66bb885a4d9a5eb7a33de21703
Rubi Leandro <93342175+rubileandro@users.noreply.github.com>
2022-06-24 18:44:32 +0200

    Update guess_the_number.py

--- guess_the_number.py
+++ guess_the_number.py
@@ -17,12 +17,32 @@ print("Welcome to the number guessing game!")
 ANSWER = randint(1, 100)
 print(ANSWER)
 
+difficulty = input("Choose a difficulty. Type 'e' for Easy or 'h' for Hard.")
+print(difficulty)
+#turns_remaining = 0
+if difficulty == "e":
+  turns_remaining = 10
+elif difficulty == "h":
+  turns_remaining = 5
+
 guess = int(input("Guess a number between 1-100:"))
-if guess > ANSWER:
-  print("Too high.")
-elif guess < ANSWER:
-  print("Too low.")
-elif guess == ANSWER:
-  print(f"You guessed the correct {ANSWER}, you win!")
-else:
-  print("Not a valid number.")
+
+def guess_game(): 
+  if guess > ANSWER:
+    print("Too high.")
+  elif guess < ANSWER:
+    print("Too low.")
+  elif guess == ANSWER:
+    print(f"You guessed the correct {ANSWER}, you win!")
+  else:
+    print("Not a valid number.")
+
+while turns_remaining > 0:
+  if guess != ANSWER:
+    turns_remaining -= 1
+    print(turns_remaining)
+    guess = int(input("Guess another number between 1-100:"))
+    guess_game()
+print ("game over")
+
+guess_game(guess, ANSWER)
