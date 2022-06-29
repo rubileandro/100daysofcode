2099f6b88141a02ef172ed302028f58daa93ad56
Rubi Leandro <93342175+rubileandro@users.noreply.github.com>
2022-06-29 17:59:02 +0200

    Update debugging_practice.py

--- debugging_practice.py
+++ debugging_practice.py
@@ -9,7 +9,26 @@ my_function()
 
 def my_function():
   # stop is ommited with range(). Therefore, i never reaches 20.
-  for i in range(1, 21):
+  for i in range(1, 20):
     if i == 20:
       print("You got it")
 my_function()
+
+# # Reproduce the Bug
+from random import randint
+dice_imgs = ["❶", "❷", "❸", "❹", "❺", "❻"]
+dice_num = randint(6, 7)
+print(dice_imgs[dice_num])
+
+# Or Reproduce the Bug
+from random import randint
+dice_imgs = ["❶", "❷", "❸", "❹", "❺", "❻"]
+dice_num = 6
+print(dice_imgs[dice_num])
+
+# Reproduce the bug fixed
+# # Reproduce the Bug
+from random import randint
+dice_imgs = ["❶", "❷", "❸", "❹", "❺", "❻"]
+dice_num = randint(0, 5)
+print(dice_imgs[dice_num])
