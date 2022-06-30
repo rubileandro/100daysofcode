d48149cf05e1b8ba161f9cfde14d91a08840fa13
Rubi Leandro <93342175+rubileandro@users.noreply.github.com>
2022-06-30 07:33:40 +0200

    Update debugging_practice.py

--- debugging_practice.py
+++ debugging_practice.py
@@ -32,3 +32,17 @@ from random import randint
 dice_imgs = ["❶", "❷", "❸", "❹", "❺", "❻"]
 dice_num = randint(0, 5)
 print(dice_imgs[dice_num])
+
+# # Play Computer
+year = int(input("What's your year of birth?"))
+if year > 1980 and year < 1994:
+  print("You are a millenial.")
+elif year > 1994:
+  print("You are a Gen Z.")
+
+# # Play Computer
+year = int(input("What's your year of birth?"))
+if year > 1980 and year < 1994:
+  print("You are a millenial.")
+elif year >= 1994:
+  print("You are a Gen Z.")
