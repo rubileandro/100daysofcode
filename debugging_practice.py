082718994ca5edd8d5d99b03fb1b08bd9893e14e
Rubi Leandro <93342175+rubileandro@users.noreply.github.com>
2022-07-02 13:10:05 +0200

    Update debugging_practice.py

--- debugging_practice.py
+++ debugging_practice.py
@@ -74,3 +74,23 @@ total_words = pages * word_per_page
 print(f"pages = {pages}")
 print(f"word_per_page = {word_per_page}")
 print(total_words)
+
+ #Use a Debugger
+# def mutate(a_list):
+#   b_list = []
+#   for item in a_list:
+#     new_item = item * 2
+#   b_list.append(new_item)
+#   print(b_list)
+
+# mutate([1,2,3,5,8,13])
+
+# #Use a Debugger
+def mutate(a_list):
+  b_list = []
+  for item in a_list:
+    new_item = item * 2
+    b_list.append(new_item)
+  print(b_list)
+
+mutate([1,2,3,5,8,13])
