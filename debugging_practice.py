bb48706c6a69cfc77cc273f5f61c8ffb3b34220e
Rubi Leandro <93342175+rubileandro@users.noreply.github.com>
2022-07-02 09:29:18 +0200

    Update debugging_practice.py

--- debugging_practice.py
+++ debugging_practice.py
@@ -56,3 +56,21 @@ elif year >= 1994:
 age = int(input("How old are you?"))
 if age > 18:
   print("You can drive at age {age}.")
+  
+# #Print is Your Friend
+# pages = 0
+# word_per_page = 0
+# pages = int(input("Number of pages: "))
+# word_per_page == int(input("Number of words per page: "))
+# total_words = pages * word_per_page
+# print(total_words)
+
+#Print is Your Friend
+pages = 0
+word_per_page = 0
+pages = int(input("Number of pages: "))
+word_per_page = int(input("Number of words per page: "))
+total_words = pages * word_per_page
+print(f"pages = {pages}")
+print(f"word_per_page = {word_per_page}")
+print(total_words)
