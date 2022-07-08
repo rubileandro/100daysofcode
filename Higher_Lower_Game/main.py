97d770de565885bbce4a348b690944d1318a6031
Rubi Leandro <93342175+rubileandro@users.noreply.github.com>
2022-07-08 10:16:19 +0200

    Update main.py

--- Higher_Lower_Game/main.py
+++ Higher_Lower_Game/main.py
@@ -5,6 +5,39 @@ from art import vs
 
 from game_data import data
 
-from replit import clear
+#from replit import clear
+
+# TODO 1: print logo
 
 print(logo)
+print("Compare A:")
+
+
+# TODO 2: print vs
+
+print(vs)
+print("Against B:")
+print("Who has more followers? Type 'A' or 'B': ")
+
+
+# TODO 3: select random dictionary from list and print in formatted way under logo - assign A
+
+
+## status of guess when right
+score = 0
+#print(f"You're right! Current score is: {score}")
+
+## status of guess when wrong
+#print(f"Sorry, that's wrong. Final score is: {score}")
+
+
+
+# TODO 4: select another random disctionary from list and print in formatted way under vs - assign B
+
+
+# TODO 5: ask user to select A or B
+player_choice = input("Choose A or B: ")
+
+
+
+# TODO 6: compare follower account from A and B and determine which is higher. If user's guess matches the result.....
