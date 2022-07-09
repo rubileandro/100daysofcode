1f6debfde56ad0c4045eb3d74858afe8f7f69b53
Rubi Leandro <93342175+rubileandro@users.noreply.github.com>
2022-07-09 19:59:46 +0200

    Update main.py

--- Higher_Lower_Game/main.py
+++ Higher_Lower_Game/main.py
@@ -17,12 +17,29 @@ print("Compare A:")
 
 print(vs)
 print("Against B:")
+
 print("Who has more followers? Type 'A' or 'B': ")
 
 
 # TODO 3: select random dictionary from list and print in formatted way under logo - assign A
+# def against_a():
+#   pass
 
 
+def against_b():
+  random_selection = random.choice(data)
+  # print(random_selection)
+  random_index = data.index(random_selection)
+  random_name = data[random_index]['name']
+  random_description = data[random_index]['description']
+  random_country = data[random_index]['country']
+  print(f"{random_name}, a {random_description}, from {random_country}.")
+
+
+against_b()
+
+#against_b
+  
 ## status of guess when right
 score = 0
 #print(f"You're right! Current score is: {score}")
@@ -36,8 +53,8 @@ score = 0
 
 
 # TODO 5: ask user to select A or B
-player_choice = input("Choose A or B: ")
-
+player_choice = input("Choose A or B: ").lower()
+print(player_choice)
 
 
 # TODO 6: compare follower account from A and B and determine which is higher. If user's guess matches the result.....
