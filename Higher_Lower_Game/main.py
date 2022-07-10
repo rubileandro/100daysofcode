e6ef0c2378ca85efb72e9dc9ff93f4886b3eb3f2
Rubi Leandro <93342175+rubileandro@users.noreply.github.com>
2022-07-10 09:48:44 +0200

    Update main.py

--- Higher_Lower_Game/main.py
+++ Higher_Lower_Game/main.py
@@ -10,22 +10,31 @@ from game_data import data
 # TODO 1: print logo
 
 print(logo)
-print("Compare A:")
 
+## Global variables?
+random_selection_a = random.choice(data)
+random_selection_b = random.choice(data)
+
+# Compare A function
+def against_a():
+  random_selection = random.choice(data)
+  # print(random_selection)
+  random_index = data.index(random_selection)
+  random_name = data[random_index]['name']
+  random_description = data[random_index]['description']
+  random_country = data[random_index]['country']
+  print(f"Compare A: {random_name}, a {random_description}, from {random_country}.")
+  
+against_a()
 
 # TODO 2: print vs
 
 print(vs)
-print("Against B:")
-
-print("Who has more followers? Type 'A' or 'B': ")
 
 
 # TODO 3: select random dictionary from list and print in formatted way under logo - assign A
-# def against_a():
-#   pass
 
-
+# Compare B function
 def against_b():
   random_selection = random.choice(data)
   # print(random_selection)
@@ -33,15 +42,28 @@ def against_b():
   random_name = data[random_index]['name']
   random_description = data[random_index]['description']
   random_country = data[random_index]['country']
-  print(f"{random_name}, a {random_description}, from {random_country}.")
+  # follower count for comparison 
+  b_followers = data[random_index]['follower_count']
+  print(f"Compare B: {random_name}, a {random_description}, from {random_country}.")
 
 
 against_b()
 
+# TODO 5: ask user to select A or B
+player_choice = input("Who has more followers? Type 'A' or 'B': ").lower()
+print(player_choice)
 #against_b
   
 ## status of guess when right
 score = 0
+
+# Compare function
+def compare_ab(a, b_followers):
+  if b_followers > a_followers:
+    return 
+  elif b_followers 
+
+
 #print(f"You're right! Current score is: {score}")
 
 ## status of guess when wrong
@@ -52,9 +74,7 @@ score = 0
 # TODO 4: select another random disctionary from list and print in formatted way under vs - assign B
 
 
-# TODO 5: ask user to select A or B
-player_choice = input("Choose A or B: ").lower()
-print(player_choice)
+
 
 
 # TODO 6: compare follower account from A and B and determine which is higher. If user's guess matches the result.....
