fca9a2ae92c40a87cfc1e6e91172377de7e60529
Rubi Leandro <93342175+rubileandro@users.noreply.github.com>
2022-07-18 17:41:59 +0200

    Create higher_lower_guided.py

---
+++ higher_lower_guided.py
@@ -0,0 +1,31 @@
+from art import logo
+from art import vs
+from game_data import data
+import random
+
+# Display art 
+print(logo)
+print(vs)
+
+# Generate a random account from the game data.
+account_a = random.choice(data)
+account_b = random.choice(data)
+
+# Format the account data into printable format.
+
+# Ask user for a guess
+
+# Check if user is correct
+## Get follower count of each account.
+## Use if statement to check if user is correct.
+
+# Give user feedback on thier guess.
+
+# Score keeping.
+
+# Make the game repeatable.
+
+# Making teh account at position B become the next account at position A 
+
+# Clear the screen between rounds.
+
