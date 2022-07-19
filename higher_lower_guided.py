1f65fc5879522906cdc429237d6d43e5a86ac8f0
Rubi Leandro <93342175+rubileandro@users.noreply.github.com>
2022-07-19 06:16:27 +0200

    Update higher_lower_guided.py

--- higher_lower_guided.py
+++ higher_lower_guided.py
@@ -3,6 +3,13 @@ from art import vs
 from game_data import data
 import random
 
+def format_data(account):
+  """Format the account data into printable format."""
+  account_name = account["name "]
+  account_descr = account["description"]
+  account_country = account["country"]
+  return f"{account_name}, a {account_descr}, from {account_country}"
+
 # Display art 
 print(logo)
 print(vs)
@@ -10,8 +17,11 @@ print(vs)
 # Generate a random account from the game data.
 account_a = random.choice(data)
 account_b = random.choice(data)
+if account_a == account_b:
+  account_b = random.choice(data)
 
-# Format the account data into printable format.
+print(f"Compare A: {format_data(account_a)}")
+print(f"Compare B: {format_data(account_b)}")
 
 # Ask user for a guess
 
