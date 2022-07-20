2283201af488497b28f88065000f3030a8f4a087
Rubi Leandro <93342175+rubileandro@users.noreply.github.com>
2022-07-20 18:56:11 +0200

    Update higher_lower_guided.py

--- higher_lower_guided.py
+++ higher_lower_guided.py
@@ -1,18 +1,23 @@
-from art import logo
-from art import vs
+from art import logo, vs
 from game_data import data
 import random
 
 def format_data(account):
-  """Format the account data into printable format."""
-  account_name = account["name "]
+  """Takes the account data and returns the printable format."""
+  account_name = account["name"]
   account_descr = account["description"]
   account_country = account["country"]
   return f"{account_name}, a {account_descr}, from {account_country}"
 
+def check_answer(guess, a_followers, b_followers):
+  """Take the user guess and follower counts and returns if they got it right."""
+  if a_followers > b_followers:
+    return guess == "a"
+  else:
+    return guess == "b"
+  
 # Display art 
 print(logo)
-print(vs)
 
 # Generate a random account from the game data.
 account_a = random.choice(data)
@@ -21,13 +26,18 @@ if account_a == account_b:
   account_b = random.choice(data)
 
 print(f"Compare A: {format_data(account_a)}")
+print(vs)
 print(f"Compare B: {format_data(account_b)}")
 
 # Ask user for a guess
+guess = input("Who has more followers? Type 'A' or 'B': ").lower()
 
 # Check if user is correct
 ## Get follower count of each account.
-## Use if statement to check if user is correct.
+a_follower_count = account_a["follower_count"]
+b_follower_count = account_b["follower_count"]
+
+
 
 # Give user feedback on thier guess.
 
