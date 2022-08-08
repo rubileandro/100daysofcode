c60509be6aa355cd76d0074ad806e73f62907f4c
Rubi Leandro <93342175+rubileandro@users.noreply.github.com>
2022-08-08 12:23:48 +0200

    Update main.py

--- turtle/main.py
+++ turtle/main.py
@@ -1,13 +1,22 @@
 # import another_module
 # print(another_module.another_variable)
 #
-from turtle import Turtle, Screen
-timmy = Turtle()
-print(timmy)
-timmy.shape("turtle")
-timmy.color("coral")
-timmy.forward(100)
+# from turtle import Turtle, Screen
+# timmy = Turtle()
+# print(timmy)
+# timmy.shape("turtle")
+# timmy.color("coral")
+# timmy.forward(100)
+#
+# my_screen = Screen()
+# print(my_screen.canvheight)
+# my_screen.exitonclick()
 
-my_screen = Screen()
-print(my_screen.canvheight)
-my_screen.exitonclick()
+from prettytable import PrettyTable
+table = PrettyTable()
+table.add_column("Pokemon Name",["Pikachu","Squirtle","Charmander"])
+table.add_column("Type",["Electric","Water","Fire"])
+# table.align["Pokemon Name"] = "l"
+# table.align["Type"] = "l"
+table.align = "l"
+print(table)
