893f7d1fe73ee9457ac1c458717b424c0788449d
Rubi Leandro <93342175+rubileandro@users.noreply.github.com>
2022-08-09 13:27:08 +0200

    Update main.py

--- OPP_coffee_machine/main.py
+++ OPP_coffee_machine/main.py
@@ -1,3 +1,9 @@
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+
+choice = input("What would you like? (espresso/latte/cappuccino): ")
+if choice == "report":
+    coffee_report = CoffeeMaker()
+    coffee_report.report()
+    print(coffee_report)
