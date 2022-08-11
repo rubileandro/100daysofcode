4c5bca89137664d3600202663fcd35eaee6d2a39
Rubi Leandro <93342175+rubileandro@users.noreply.github.com>
2022-08-11 07:46:20 +0200

    Update main.py

--- OPP_coffee_machine/main.py
+++ OPP_coffee_machine/main.py
@@ -2,8 +2,20 @@ from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
+menu = Menu()
+coffee_maker = CoffeeMaker()
+money_machine = MoneyMachine()
+
+
 choice = input("What would you like? (espresso/latte/cappuccino): ")
 if choice == "report":
     coffee_report = CoffeeMaker()
     coffee_report.report()
     print(coffee_report)
+else:
+    drink = menu.find_drink(choice)
+
+
+
+if coffee_maker.is_resource_sufficient(drink):
+    coffee_maker.make_coffee(drink)
