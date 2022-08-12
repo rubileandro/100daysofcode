ddb36ed3b4944118febb363f5856d817fd26c84b
Rubi Leandro <93342175+rubileandro@users.noreply.github.com>
2022-08-12 18:01:35 +0200

    Update main.py

--- OPP_coffee_machine/main.py
+++ OPP_coffee_machine/main.py
@@ -7,15 +7,18 @@ coffee_maker = CoffeeMaker()
 money_machine = MoneyMachine()
 
 
+is_on = True
 choice = input("What would you like? (espresso/latte/cappuccino): ")
 if choice == "report":
     coffee_report = CoffeeMaker()
     coffee_report.report()
     print(coffee_report)
+elif choice == "off":
+    is_on = False
+
 else:
     drink = menu.find_drink(choice)
+    if coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
+        #money_machine.process_coins()
 
-
-
-if coffee_maker.is_resource_sufficient(drink):
-    coffee_maker.make_coffee(drink)
+        coffee_maker.make_coffee(drink)
