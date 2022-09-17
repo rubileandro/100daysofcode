b9f94091b8b7f9a81d940236b4783d3115a5f8a9
Rubi Leandro <93342175+rubileandro@users.noreply.github.com>
2022-09-17 05:59:38 +0200

    Update ten_shapes.py

--- turtle_graphics/ten_shapes.py
+++ turtle_graphics/ten_shapes.py
@@ -1,50 +1,21 @@
-from turtle import Turtle, Screen
-from random import randint
+import random
+import turtle as t
 
-timmy = Turtle()
+
+timmy = t.Turtle()
 timmy.shape("turtle")
-# timmy.color("DarkSeaGreen4")
+
+colors = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue", "LightSeaGreen", "wheat", "SlateGray", "SeaGreen"]
 
 
-# Triangle 360 / 3 = 120
-for _ in range(3):
-    timmy.right(120)
-    timmy.forward(100)
+def draw_shape(num_sides):
+    angle = 360 / num_sides
+    for _ in range(num_sides):
+        timmy.forward(100)
+        timmy.right(angle)
 
-# Square 360 / 4 = 90
-for _ in range(4):
-    timmy.right(90)
-    timmy.forward(100)
 
-# Pentagon 360 / 5 = 72
-for _ in range(5):
-    timmy.right(72)
-    timmy.forward(100)
-
-# Hexagon 360 / 6 = 60
-for _ in range(6):
-    timmy.right(60)
-    timmy.forward(100)
-
-# Heptagon 360 / 7 = 51.4
-for _ in range(7):
-    timmy.right(51.4)
-    timmy.forward(100)
-
-# Octagon 360 / 8 = 45
-for _ in range(8):
-    timmy.right(45)
-    timmy.forward(100)
-
-# Nonagon 360 / 9 = 40
-for _ in range(9):
-    timmy.right(40)
-    timmy.forward(100)
-
-# Decagon 360 / 10 = 36
-for _ in range(10):
-    timmy.right(36)
-    timmy.forward(100)
-
-screen = Screen()
-screen.exitonclick()
+for shape_side_n in range(3, 11):
+    timmy.color(random.choice(colors))
+    draw_shape(shape_side_n)
+    
