e5dc9ab05523ca454851b72ec4ad3e4ded5e4084
Rubi Leandro <93342175+rubileandro@users.noreply.github.com>
2022-10-03 15:22:49 +0200

    Update main.py

--- snake_game/main.py
+++ snake_game/main.py
@@ -12,6 +12,12 @@ screen.tracer(0)
 
 snake = Snake()
 
+screen.listen()
+screen.onkey(snake.up,"Up")
+screen.onkey(snake.down,"Down")
+screen.onkey(snake.left,"Left")
+screen.onkey(snake.right,"Right")
+
 
 game_is_on = True
 while game_is_on:
