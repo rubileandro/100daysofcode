a2df23ef146d3f8fc01927a4e5a5814916de4722
Rubi Leandro <93342175+rubileandro@users.noreply.github.com>
2022-10-08 11:43:05 +0200

    Update food.py

--- snake_game/food.py
+++ snake_game/food.py
@@ -9,8 +9,12 @@ class Food(Turtle):
         self.shape("circle")
         self.penup()
         self.shapesize(stretch_len=0.5, stretch_wid=0.5)
-        self.color("blue")
+        self.color("light sea green")
         self.speed("fastest")
+        self.refresh()
+
+    def refresh(self):
         random_x = random.randint(-280, 280)
         random_y = random.randint(-280, 280)
         self.goto(random_x, random_y)
+        
