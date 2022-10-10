a15aaf407b074dadb6f2f899de7d0a414861fe5d
Rubi Leandro <93342175+rubileandro@users.noreply.github.com>
2022-10-10 11:46:44 +0200

    Update main.py

--- snake_game/main.py
+++ snake_game/main.py
@@ -2,7 +2,7 @@ from turtle import Screen
 from snake import Snake
 import time
 from food import Food
-
+from scoreboard import Scoreboard
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -13,10 +13,9 @@ screen.tracer(0)
 
 snake = Snake()
 food = Food()
+scoreboard = Scoreboard()
 
 screen.listen()
-
-
 screen.onkey(snake.up, "Up")
 screen.onkey(snake.down, "Down")
 screen.onkey(snake.left, "Left")
@@ -33,6 +32,11 @@ while game_is_on:
     if snake.head.distance(food) < 15:
         # print("nom nom nom")
         food.refresh()
+        scoreboard.increase_score()
+    # Detect collion with wall
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        game_is_on = False
+        scoreboard.game_over()
 
 
 screen.exitonclick()
