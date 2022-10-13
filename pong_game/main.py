21f5fb9e047841101e3c70c25881feed6cbde151
Rubi Leandro <93342175+rubileandro@users.noreply.github.com>
2022-10-13 10:20:52 +0200

    Update main.py

--- pong_game/main.py
+++ pong_game/main.py
@@ -1,8 +1,24 @@
-from turtle import Screen
+from turtle import Turtle, Screen
+from paddle import Paddle
 
 screen = Screen()
 screen.bgcolor("black")
 screen.setup(width=800, height=600)
 screen.title("Pong")
+screen.tracer(0)
+
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
+
+
+screen.listen()
+screen.onkey(r_paddle.go_up, "Up")
+screen.onkey(r_paddle.go_down, "Down")
+screen.onkey(l_paddle.go_up, "w")
+screen.onkey(l_paddle.go_down, "s")
+
+game_is_on = True
+while game_is_on:
+    screen.update()
 
 screen.exitonclick()
