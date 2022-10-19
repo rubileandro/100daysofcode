68ad195fa725a16c98c265f4a8f1231783ab0c2e
Rubi Leandro <93342175+rubileandro@users.noreply.github.com>
2022-10-19 18:15:33 +0200

    Update main.py

--- pong_game/main.py
+++ pong_game/main.py
@@ -1,5 +1,7 @@
 from turtle import Turtle, Screen
 from paddle import Paddle
+from ball import Ball
+import time
 
 screen = Screen()
 screen.bgcolor("black")
@@ -9,6 +11,7 @@ screen.tracer(0)
 
 r_paddle = Paddle((350, 0))
 l_paddle = Paddle((-350, 0))
+ball = Ball()
 
 
 screen.listen()
@@ -19,6 +22,12 @@ screen.onkey(l_paddle.go_down, "s")
 
 game_is_on = True
 while game_is_on:
+    time.sleep(0.1)
     screen.update()
+    ball.move()
+
+    # Detect collision with wall
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce()
 
 screen.exitonclick()
