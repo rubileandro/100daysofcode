6c529b5401979dba5c4fcae7f82971aa9a078981
Rubi Leandro <93342175+rubileandro@users.noreply.github.com>
2022-10-26 21:03:24 +0400

    Update main.py

--- turlte-crossing/main.py
+++ turlte-crossing/main.py
@@ -23,5 +23,10 @@ while game_is_on:
     car_manager.create_car()
     car_manager.move_cars()
 
+    # Detect collision with car
+    for car in car_manager.all_cars:
+        if car.distance(player) < 20:
+            game_is_on = False
+
 
 screen.exitonclick()
