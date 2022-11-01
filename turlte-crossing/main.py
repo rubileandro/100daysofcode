05e436ebb8300fdf05af4e986c759b1ffd96287a
Rubi Leandro <93342175+rubileandro@users.noreply.github.com>
2022-11-01 18:12:11 +0400

    Update main.py

--- turlte-crossing/main.py
+++ turlte-crossing/main.py
@@ -8,9 +8,9 @@ screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
 
-
 player = Player()
 car_manager = CarManager()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(player.go_up, "Up")
@@ -27,6 +27,12 @@ while game_is_on:
     for car in car_manager.all_cars:
         if car.distance(player) < 20:
             game_is_on = False
+            scoreboard.game_over()
 
+    # Detect successful crossing
+    if player.is_at_finish_line():
+        player.go_to_start()
+        car_manager.level_up()
+        scoreboard.increase_level()
 
 screen.exitonclick()
