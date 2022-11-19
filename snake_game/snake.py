c0f6823d806b8d26d859cea4f874e6b1a29666ef
Rubi Leandro <93342175+rubileandro@users.noreply.github.com>
2022-11-19 13:49:07 +0400

    Update snake.py

--- snake_game/snake.py
+++ snake_game/snake.py
@@ -1,5 +1,5 @@
 from turtle import Turtle
-STARTING_POSITIONS = starting_positions = [(0, 0), (-20, 0), (-40, 0)]
+STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
 MOVE_DISTANCE = 20
 UP = 90
 DOWN = 270
@@ -19,11 +19,18 @@ class Snake:
             self.add_segment(position)
 
     def add_segment(self, position):
-        new_segment = Turtle(shape="square")
+        new_segment = Turtle("square")
         new_segment.color("white")
         new_segment.penup()
         new_segment.goto(position)
         self.segments.append(new_segment)
+
+    def reset(self):
+        for seg in self.segments:
+            seg.goto(1000, 1000)
+        self.segments.clear()
+        self.create_snake()
+        self.head = self.segments[0]
 
     def extend(self):
         self.add_segment(self.segments[-1].position())
