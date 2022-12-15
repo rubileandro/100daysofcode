c6832fc44d12be284327c68b0cbe3a761a74918a
Rubi Leandro <93342175+rubileandro@users.noreply.github.com>
2022-12-15 17:15:55 +0400

    Update main.py

--- pandas-basics/main.py
+++ pandas-basics/main.py
@@ -1,6 +1,6 @@
-import pandas
+# import pandas
 
-data = pandas.read_csv("weather_data.csv")
+# data = pandas.read_csv("weather_data.csv")
 # print(type(data))
 # print(type(data["temp"]))
 
@@ -28,9 +28,27 @@ data = pandas.read_csv("weather_data.csv")
 # print(monday_temp_fahrenheit)
 
 #Create a dataframe from scratch
+# data_dict = {
+#     "students": ["Amy", "James","Angela"],
+#     "scores": [76, 56, 65]
+# }
+# data = pandas.DataFrame(data_dict)
+# data.to_csv("new_data.csv")
+
+import pandas
+
+data = pandas.read_csv("2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv")
+grey_squirrels_count = len(data[data["Primary Fur Color"] == "Gray"])
+black_squirrels_count = len(data[data["Primary Fur Color"] == "Black"])
+cinnamon_squirrels_count = len(data[data["Primary Fur Color"] == "Cinnamon"])
+print(grey_squirrels_count)
+print(cinnamon_squirrels_count)
+print(black_squirrels_count)
+
 data_dict = {
-    "students": ["Amy", "James","Angela"],
-    "scores": [76, 56, 65]
+    "Fur Color": ["Gray", "Cinnamon", "Black"],
+    "Count": [grey_squirrels_count, cinnamon_squirrels_count, black_squirrels_count]
 }
-data = pandas.DataFrame(data_dict)
-data.to_csv("new_data.csv")
+
+df = pandas.DataFrame(data_dict)
+df.to_csv("squirrel_count.csv")
