a5ac9c0738d44cf42e7abf1c43167614710e73e1
Rubi Leandro <93342175+rubileandro@users.noreply.github.com>
2022-12-19 20:16:37 +0400

    Update main.py

--- us-states_game/main.py
+++ us-states_game/main.py
@@ -1,4 +1,5 @@
 import turtle
+import pandas
 
 screen = turtle.Screen()
 screen.title("U.S States Game")
@@ -6,14 +7,22 @@ image = "blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
 
+data = pandas.read_csv("50_states.csv")
+all_states = data.state.to_list()
+guessed_states = []
 
-# def get_mouse_click_coor(x, y):
-#     print(x, y)
-#
-#
-# turtle.onscreenclick(get_mouse_click_coor)
-#
-# turtle.mainloop()
+while len(guessed_states) < 50:
+    answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct",
+                                    prompt="What's another states name?").title()
+    print(answer_state)
 
-answer_state = screen.textinput(title="Guess the State", prompt="What's another states name?")
-print(answer_state)
+    if answer_state in all_states:
+        guessed_states.append(answer_state)
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = data[data.state == answer_state]
+        t.goto(int(state_data.x), int(state_data.y))
+        t.write(answer_state)
+
+screen.exitonclick()
