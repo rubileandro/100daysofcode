e01b7c6811516b021d789c0f3d118b81cb744695
Rubi Leandro <93342175+rubileandro@users.noreply.github.com>
2022-12-21 17:35:30 +0400

    Update main.py

--- us-states_game/main.py
+++ us-states_game/main.py
@@ -16,6 +16,14 @@ while len(guessed_states) < 50:
                                     prompt="What's another states name?").title()
     print(answer_state)
 
+    if answer_state == "Exit":
+        missing_states = []
+        for state in all_states:
+            if state not in guessed_states:
+                missing_states.append(state)
+        new_data = pandas.DataFrame(missing_states)
+        new_data.to_csv("states_to_learn.csv")
+        break
     if answer_state in all_states:
         guessed_states.append(answer_state)
         t = turtle.Turtle()
@@ -24,5 +32,3 @@ while len(guessed_states) < 50:
         state_data = data[data.state == answer_state]
         t.goto(int(state_data.x), int(state_data.y))
         t.write(answer_state)
-
-screen.exitonclick()
