4b0b1e2ecc4a648390d9676812e49677c84d43fe
Rubi Leandro <93342175+rubileandro@users.noreply.github.com>
2023-02-06 11:13:00 +0400

    Create main.py

---
+++ catching exceptions/main.py
@@ -0,0 +1,26 @@
+# # FileNotFoundError
+# with open("a_file.txt") as file:
+#     file.read()
+
+# Attempting to take file not found exception,
+# and let's see how we make it safer by catching that exception.
+try:
+    file = open("a_file.txt")
+    a_dictionary = {"key": "value"}
+    print(a_dictionary["sdfsdf"])
+except FileNotFoundError:
+    file = open("a_file.txt", "w")
+    file.write("Something")
+    # print("There as an error.")
+
+# # KeyError
+# a_dictionary = {"key": "value"}
+# value = a_dictionary["non_existent_key"]
+
+# # IndexError
+# fruit_list = ["Apple", "Banana", "Pear"]
+# fruit = fruit_list[3]
+
+# # TypeError
+# text = "abc"
+# print(text + 5)
