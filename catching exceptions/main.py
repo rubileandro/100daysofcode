2af97221b34be00ba107d73d95ebe1e58e4e2740
Rubi Leandro <93342175+rubileandro@users.noreply.github.com>
2023-02-07 11:16:05 +0400

    Update main.py

--- catching exceptions/main.py
+++ catching exceptions/main.py
@@ -7,11 +7,20 @@
 try:
     file = open("a_file.txt")
     a_dictionary = {"key": "value"}
-    print(a_dictionary["sdfsdf"])
+    print(a_dictionary["key"])
 except FileNotFoundError:
     file = open("a_file.txt", "w")
     file.write("Something")
     # print("There as an error.")
+except KeyError as error_message:
+    print(f"The key {error_message} does not exist.")
+else:
+    content = file.read()
+    print(content)
+finally:
+    file.close()
+    print("File was closed.")
+
 
 # # KeyError
 # a_dictionary = {"key": "value"}
@@ -24,3 +33,4 @@ except FileNotFoundError:
 # # TypeError
 # text = "abc"
 # print(text + 5)
+
