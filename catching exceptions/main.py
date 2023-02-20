681a3cfaaec4b47ba2ca083ed281bd7d20904da6
Rubi Leandro <93342175+rubileandro@users.noreply.github.com>
2023-02-20 11:33:50 +0400

    Update main.py

--- catching exceptions/main.py
+++ catching exceptions/main.py
@@ -4,22 +4,22 @@
 
 # Attempting to take file not found exception,
 # and let's see how we make it safer by catching that exception.
-try:
-    file = open("a_file.txt")
-    a_dictionary = {"key": "value"}
-    print(a_dictionary["key"])
-except FileNotFoundError:
-    file = open("a_file.txt", "w")
-    file.write("Something")
-    # print("There as an error.")
-except KeyError as error_message:
-    print(f"The key {error_message} does not exist.")
-else:
-    content = file.read()
-    print(content)
-finally:
-    file.close()
-    print("File was closed.")
+# try:
+#     file = open("a_file.txt")
+#     a_dictionary = {"key": "value"}
+#     print(a_dictionary["key"])
+# except FileNotFoundError:
+#     file = open("a_file.txt", "w")
+#     file.write("Something")
+#     # print("There as an error.")
+# except KeyError as error_message:
+#     print(f"The key {error_message} does not exist.")
+# else:
+#     content = file.read()
+#     print(content)
+# finally:
+#     file.close()
+#     print("File was closed.")
 
 
 # # KeyError
@@ -33,4 +33,24 @@ finally:
 # # TypeError
 # text = "abc"
 # print(text + 5)
+
+facebook_posts = [
+    {'Likes': 21, 'Comments': 2}, 
+    {'Likes': 13, 'Comments': 2, 'Shares': 1}, 
+    {'Likes': 33, 'Comments': 8, 'Shares': 3}, 
+    {'Comments': 4, 'Shares': 2}, 
+    {'Comments': 1, 'Shares': 1}, 
+    {'Likes': 19, 'Comments': 3}
+]
+
+total_likes = 0
+
+for post in facebook_posts:
+    try:
+        total_likes = total_likes + post['Likes']
+    except KeyError:
+        pass
+
+
+print(total_likes)
 
