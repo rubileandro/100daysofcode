883ab91c017b64e64b2597b247d28a4b4df75ca6
Rubi Leandro <93342175+rubileandro@users.noreply.github.com>
2023-05-15 11:05:42 +0400

    Update main.py

--- flash-card-project/main.py
+++ flash-card-project/main.py
@@ -9,7 +9,9 @@ window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 canvas = Canvas(width=800, height=526)
 card_front_img = PhotoImage(file="images/card_front.png")
 canvas.create_image(400, 263, image=card_front_img)
+canvas.create_text(400, 150, text="Title", font=("Ariel", 40, "italic"))
+canvas.create_text(400, 263, text="word", font=("Ariel", 60, "bold"))
+canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
 canvas.grid(row=0, column=0)
 
 window.mainloop()
-
