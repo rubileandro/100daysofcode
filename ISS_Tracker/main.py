f38899b162bcd70e0118ff4c0bcea81d917a03dc
Rubi Leandro <93342175+rubileandro@users.noreply.github.com>
2023-12-05 16:36:27 +0400

    Update main.py
    Longitude and Latitude for Mauritius. Program now has the UCT time for sunrise and sunset.

--- ISS_Tracker/main.py
+++ ISS_Tracker/main.py
@@ -1,13 +1,43 @@
 import requests
+import datetime
 
-response = requests.get(url="http://api.open-notify.org/iss-now.json")
+# Mauritius
+MY_LAT = -20.348404
+MY_LONG = 57.552151
+
+# London
+# MY_LAT = 51.507351
+# MY_LONG = -0.127758
+
+
+# response = requests.get(url="http://api.open-notify.org/iss-now.json")
+# response.raise_for_status()
+#
+# data = response.json()
+# print(data)
+#
+# longitude = data["iss_position"]["longitude"]
+# latitude = data["iss_position"]["latitude"]
+#
+# iss_position = (longitude, latitude)
+# print(iss_position)
+
+parameters = {
+    "lat": MY_LAT,
+    "lng": MY_LONG,
+    "formatted": 0,
+}
+
+response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
 response.raise_for_status()
-
 data = response.json()
-print(data)
+# print(data)
 
-longitude = data["iss_position"]["longitude"]
-latitude = data["iss_position"]["latitude"]
+sunrise = data["results"]["sunrise"].split("T")[1].split(":")[0]
+sunset = data["results"]["sunset"].split("T")[1].split(":")[0]
 
-iss_position = (longitude, latitude)
-print(iss_position)
+print(sunrise)
+print(sunset)
+
+time_now = datetime.datetime.now()
+print(time_now)
